52d78ce8037f4d0f3e1da0de1896952fa71bf6c1
davidsonbr12 <davidsonbr12@gmail.com>
2025-07-11 12:24:31 -0700

    write_file.py function added to functions, tests.py updated to test new function

---
+++ functions/write_file.py
@@ -0,0 +1,15 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    file_dir = os.path.dirname(abs_file_path)
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    #write to file
+    with open(abs_file_path, 'w') as f:
+        f.write(content)
+    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
